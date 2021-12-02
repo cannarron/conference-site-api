e9538f2dc30aa21aa58c38d209cc65c8145ca5c5
Cannarron <cannarron@gmail.com>
2021-12-02 11:57:12 +0100

    (chore): API structure

---
+++ src/main/java/com/nessa/conferencesite/ConferenceSiteApplication.java
@@ -0,0 +1,13 @@
+package com.nessa.conferencesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSiteApplication.class, args);
+	}
+
+}
